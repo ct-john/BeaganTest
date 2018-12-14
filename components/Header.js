1193e74ba49dc9e3b6660255a75026d0d2d0be20
John Beagan <john.beagan@constanttherapy.com>
2018-12-14 13:13:41 -0500

    pm2 - test 7 - light yellow

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,15 @@ const linkStyle = {
 
 const Header = () => (
     <div>
+      <h1>test 67/h1>
+    <div>
         <Link href="/">
           <a style={linkStyle}>Home</a>
         </Link>
         <Link href="/about">
           <a style={linkStyle}>About</a>
         </Link>
+    </div>
     </div>
 )
 
