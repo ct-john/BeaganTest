b139ba96c7a980aea9f9dfd1d642cebeeee9be51
“ct-john” <john.beagan@constanttherapy.com>
2019-01-25 16:30:31 -0500

    yellowish

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const linkStyle = {
 
 const Header = () => (
     <div>
-      <h1>lightgray 1.0</h1>
+      <h1>yellowish 1.0</h1>
     <div>
         <Link href="/">
           <a style={linkStyle}>Home</a>
